7624daca560fa12529cab154b2343e5d1ce78e25
Peter Pham <ptrpham2468@gmail.com>
2022-04-28 12:18:27 -0500

    added the data for the rest of the tables

--- Database.sql
+++ Database.sql
@@ -5,11 +5,11 @@ USE COMPANY;
 
 DROP TABLE IF EXISTS MANAGER;
 CREATE TABLE MANAGER (
-	manager_ID CHAR(10) NOT NULL,
+    manager_ID CHAR(10) NOT NULL,
     first_name VARCHAR(20) NOT NULL,
     last_name VARCHAR(20) NOT NULL,
     department VARCHAR(20),
-    pay DECIMAL(10, 2),
+    pay DECIMAL(10 , 2 ),
     ssn CHAR(9),
     middle_name VARCHAR(20),
     CONSTRAINT pk_manager PRIMARY KEY (manager_ID)
@@ -17,63 +17,63 @@ CREATE TABLE MANAGER (
 
 DROP TABLE IF EXISTS STORE;
 CREATE TABLE STORE (
-	store_ID CHAR(5) NOT NULL,
+    store_ID CHAR(5) NOT NULL,
     manager_ID CHAR(10),
     phone_number CHAR(10),
     CONSTRAINT pk_store PRIMARY KEY (store_ID),
     CONSTRAINT fk_store_manager FOREIGN KEY (manager_ID)
-		REFERENCES MANAGER (manager_ID)
+        REFERENCES MANAGER (manager_ID)
 );
 
 DROP TABLE IF EXISTS Employee;
 CREATE TABLE Employee (
-	employee_ID CHAR(10) NOT NULL,
+    employee_ID CHAR(10) NOT NULL,
     ssn CHAR(9),
     email VARCHAR(50),
     phone_number CHAR(10),
     manager_ID CHAR(10),
-    pay DECIMAL(10, 2),
+    pay DECIMAL(10 , 2 ),
     first_name VARCHAR(20) NOT NULL,
     last_name VARCHAR(20) NOT NULL,
     middle_name VARCHAR(20),
     address VARCHAR(50),
     CONSTRAINT pk_employee PRIMARY KEY (employee_ID),
     CONSTRAINT fk_emp_manager FOREIGN KEY (manager_ID)
-		REFERENCES MANAGER (manager_ID)
+        REFERENCES MANAGER (manager_ID)
 );
 
 DROP TABLE IF EXISTS Users;
 CREATE TABLE Users (
-	customer_ID CHAR(10) NOT NULL,
+    customer_ID CHAR(10) NOT NULL,
     first_name VARCHAR(20) NOT NULL,
     last_name VARCHAR(20) NOT NULL,
     phone_num CHAR(10),
     reward_points INT,
-    address char(50),
+    address CHAR(50),
     CONSTRAINT pk_user PRIMARY KEY (customer_ID),
-    CONSTRAINT uk_address unique key (address)
+    CONSTRAINT uk_address UNIQUE KEY (address)
 );
 
 DROP TABLE IF EXISTS Tracking;
 CREATE TABLE Tracking (
-	tracking_ID CHAR(10) NOT NULL,
+    tracking_ID CHAR(10) NOT NULL,
     depart_time TIME,
     arrive_time TIME,
-    destination char(50) NOT NULL,
-    weight DECIMAL(10, 2),
-    length DECIMAL(10, 2),
-    width DECIMAL(10, 2),
+    destination CHAR(50) NOT NULL,
+    weight DECIMAL(10 , 2 ),
+    length DECIMAL(10 , 2 ),
+    width DECIMAL(10 , 2 ),
     container_ID CHAR(10),
     item_ID CHAR(10),
     customer_ID CHAR(10),
     CONSTRAINT pk_tracking PRIMARY KEY (tracking_ID),
     CONSTRAINT fk_destination FOREIGN KEY (destination)
-		REFERENCES Users (address)
+        REFERENCES Users (address)
 );
 
 DROP TABLE IF EXISTS Brand;
 CREATE TABLE Brand (
-	brand_ID CHAR(10) NOT NULL,
+    brand_ID CHAR(10) NOT NULL,
     brand_name VARCHAR(20),
     CONSTRAINT pk_brand PRIMARY KEY (brand_ID)
 );
@@ -91,20 +91,20 @@ CREATE TABLE Items (
 
 DROP TABLE IF EXISTS Purchases;
 CREATE TABLE Purchases (
-	purchase_ID CHAR(10) NOT NULL,
+    purchase_ID CHAR(10) NOT NULL,
     item_ID CHAR(10) NOT NULL,
-    amount DECIMAL(10,2),
+    amount DECIMAL(10 , 2 ),
     points_earned INT,
     tracking_ID CHAR(10),
     customer_ID CHAR(10),
     store_ID CHAR(5),
     CONSTRAINT pk_purchase PRIMARY KEY (purchase_ID),
     CONSTRAINT fk_item FOREIGN KEY (item_ID)
-		REFERENCES Items (item_ID),
-	CONSTRAINT fk_store FOREIGN KEY (store_ID)
-		REFERENCES Store (store_ID),
-	CONSTRAINT fk_customer FOREIGN KEY (customer_ID)
-		REFERENCES Users (customer_ID)
+        REFERENCES Items (item_ID),
+    CONSTRAINT fk_store FOREIGN KEY (store_ID)
+        REFERENCES Store (store_ID),
+    CONSTRAINT fk_customer FOREIGN KEY (customer_ID)
+        REFERENCES Users (customer_ID)
 );
 
 -- separator
@@ -243,9 +243,99 @@ INSERT INTO USERS VALUES
 ('5853031485', 'Alric', 'Croxton', '6017682180', '94442', '133 Corscot Way'),
 ('9059631397', 'Dwight', 'Eckersall', '7437884663', '78939', '70 4th Pass');
 
-SELECT * FROM STORE;
-SELECT * FROM MANAGER;
-SELECT * FROM EMPLOYEE;
-SELECT * FROM USERS;
+insert into Tracking values ('Sy2HuRUFX2', '2021-06-04 22:42:52', '2021-07-17 03:42:02', '7294 Tomscot Way', 13.86, 44.82, 49.12, 'sSKieYoccM', 7138517899, '6416748263'),
+('KcnSVy13UM', '2022-01-31 13:37:13', '2021-06-08 00:03:42', '7241 Laurel Point', 17.44, 18.64, 32.35, 'wB8cDMHh6f', 1696928366, '1650035298'),
+('NwipxdRJVM', '2022-02-08 05:25:11', '2021-09-22 15:52:59', '3 Union Lane', 35.36, 43.54, 42.29, 'Lar6WXvNLy', 5268792434, '6604725132'),
+('ESg15ulza2', '2021-06-13 07:29:35', '2022-02-21 19:52:39', '01182 Mallard Crossing', 8.38, 11.94, 17.84, 'Nnvc4YiaXv', 2071129437, '3203610731'),
+('wVcb3FuU66', '2022-04-22 01:55:04', '2022-01-18 02:33:20', '32 Clemons Center', 5.35, 4.12, 22.01, 'Pq4O3tMRhY', 6337558729, '8046818410'),
+('ALOfVoCNCQ', '2021-11-09 09:41:53', '2022-04-10 02:12:18', '53548 Mendota Terrace', 22.38, 47.01, 38.32, 'ARq4CCHnHQ', 8333212393, '5723601544'),
+('ytLE1oGtp6', '2021-05-16 06:19:08', '2022-04-25 09:51:17', '05 Ramsey Crossing', 33.21, 10.65, 49.96, 'Ut8rWX9n2X', 4301072480, '1960960930'),
+('Do7Y6TMjOD', '2022-01-30 02:30:06', '2021-11-25 13:52:18', '9 Tennyson Road', 32.68, 44.57, 1.78, 'eSWz41XTeM', 5701435996, '8477465037'),
+('Wp0mxqKbI2', '2021-09-15 01:05:10', '2021-11-02 00:36:25', '3740 Straubel Point', 39.78, 16.19, 46.54, 'aNqlzyoQSf', 9625451414, '5407526790'),
+('9Gm1dJBQY7', '2021-12-19 03:47:24', '2021-09-24 19:40:02', '391 Hagan Trail', 27.67, 44.62, 3.24, 'IiVOk91tiy', 9870256558, '6623572609');
 
+
+insert into brand values 
+(1847828588, 'Schmitt Inc'),
+(6584662294, 'Collier and Schultz'),
+(3026087388, 'Douglas Group'),
+(3862438533, 'Rosenbaum-Corwin'),
+(6929649207, 'Rohan and Hackett'),
+(2119654589, 'Larson Inc'),
+(6561644045, 'Hand Inc'),
+(2625280924, 'Stroman LLC'),
+(4115060737, 'Davis and Sons'),
+(7469574971, 'Thompson-MacGyver');
+
+insert into Items values 
+(3707232671, 18.27, 'Baby', 1847828588),
+(3454166092, 268.28, 'Games', 1847828588),
+(8660764238, 450.65, 'Clothing', 3026087388),
+(4681087101, 418.62, 'Home', 1847828588),
+(5159103470, 392.06, 'Shoes', 1847828588),
+(6156728892, 36.3, 'Games', 3026087388),
+(3018776604, 136.96, 'Clothing', 3026087388),
+(6127671158, 434.1, 'Kids', 1847828588),
+(9615093992, 75.66, 'Garden', 6929649207),
+(9125947316, 139.61, 'Health', 6929649207),
+(5850041484, 1.14, 'Electronics', 4115060737),
+(1657158655, 273.25, 'Industrial', 1847828588),
+(8214768679, 232.33, 'Clothing', 1847828588),
+(8214788679, 108.82, 'Shoes', 1847828588),
+(1824286070, 17.68, 'Industrial', 1847828588),
+(6195740861, 7.27, 'Clothing', 6929649207),
+(2990767662, 315.28, 'Health', 6929649207),
+(7597641937, 487.61, 'Clothing', 1847828588),
+(9119522841, 209.5, 'Industrial', 7469574971),
+(8814788679, 54.6, 'Toys', 6929649207),
+(5440060190, 154.31, 'Clothing', 7469574971),
+(9623443159, 218.35, 'Home', 2119654589),
+(4201760855, 70.34, 'Games', 3026087388),
+(9531355856, 52.01, 'Home', 2119654589),
+(6384978638, 335.51, 'Sports', 3026087388),
+(3899493156, 152.09, 'Toys', 3026087388),
+(1214788679, 408.62, 'Shoes', 3026087388),
+(6565218350, 402.67, 'Shoes', 2119654589),
+(4896698958, 463.51, 'Electronics', 6929649207),
+(3696492335, 212.6, 'Shoes', 2119654589),
+(4683351245, 411.41, 'Clothing', 6929649207),
+(6320759218, 242.17, 'Shoes', 6929649207),
+(1314988779, 90.26, 'Electronics', 6929649207),
+(8926123332, 138.03, 'Clothing', 4115060737),
+(4289133341, 100.5, 'Home', 6929649207);
+
+
+insert into Purchases values
+(6239485307, 8214788679, 52.29, 14, 'Sy2HuRUFX2', '6416748263', 12345),
+(7752829660, 8214788679, 89.56, 1, 'KcnSVy13UM', '1650035298', 12345),
+(9907479924, 8214788679, 47.26, 17, 'NwipxdRJVM', '6604725132', 12345),
+(2784128329, 8214788679, 63.97, 20, 'ESg15ulza2', '3203610731', 12345),
+(9681529911, 8214788679, 87.56, 23, 'wVcb3FuU66', '8046818410', 12345),
+(9331467664, 9125947316, 54.02, 13, 'ALOfVoCNCQ', '5723601544', 12345),
+(4145410290, 9125947316, 54.07, 13, 'ytLE1oGtp6', '1960960930', 12345),
+(5352534114, 4683351245, 97.5, 39, 'Do7Y6TMjOD', '8477465037', 12345),
+(5242677941, 4289133341, 88.23, 14, 'Wp0mxqKbI2', '5407526790', 12345),
+(2713228727, 8214788679, 31.32, 14, '9Gm1dJBQY7', '6623572609', 12345);
+
+
+SELECT 
+    *
+FROM
+    STORE;
+SELECT 
+    *
+FROM
+    MANAGER;
+SELECT 
+    *
+FROM
+    EMPLOYEE;
+SELECT 
+    *
+FROM
+    USERS;
+SELECT 
+    *
+FROM
+    Tracking;
 
